27ebffa08f037bf5e43acfda22433a469318fe02
Victoria Dunham <vkurzweg@gmail.com>
2017-04-28 11:32:36 -0700

    style resume page

--- public/src/components/Resume.js
+++ public/src/components/Resume.js
@@ -1,4 +1,8 @@
 import React from "react";
+import Nav from './common/Nav';
+import NavMobile from './common/NavMobile';
+import MediaQuery from 'react-responsive';
+
 
 // app component
 export default class Resume extends React.Component {
@@ -7,7 +11,16 @@ export default class Resume extends React.Component {
   render() {
     return (
       <div>
-        <h4>Test</h4>
+        <MediaQuery minWidth={768}>
+          <div className="resume-image">
+            <Nav />
+          </div>
+        </MediaQuery>
+        <MediaQuery maxWidth={767}>
+          <div className="resume-image-mobile">
+            <NavMobile />
+          </div>
+        </MediaQuery>
       </div>
     );
   }
