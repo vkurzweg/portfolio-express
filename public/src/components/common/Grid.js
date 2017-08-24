556932a476c536591618923ce356495157c661f4
Victoria Dunham <vkurzweg@gmail.com>
2017-08-24 14:10:34 -0700

    edit copy

--- public/src/components/common/Grid.js
+++ public/src/components/common/Grid.js
@@ -248,7 +248,7 @@ export class Grid extends React.Component {
             </StyledGridTile>
           ))}
         </GridList>
-        <p style={{ fontSize: '12px', fontStyle: 'italic', textAlign: 'center' }}>Click on any image for more info</p>
+        <p style={{ fontSize: '12px', fontStyle: 'italic', textAlign: 'center' }}>Click on image for project details</p>
         <Modal title="Silicon Beach Guitar" visible={this.state.visible0} footer={null} onCancel={this.closeModal0} style={{ top: 50 }}>
           <div className="container">
             <div className="row">
