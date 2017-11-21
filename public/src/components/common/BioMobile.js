7df4e5433f96d0961a024c4ff571669d9f9459d1
Victoria Kurzweg <vkurzweg@gmail.com>
2017-11-21 10:15:04 -0800

    edit bio

--- public/src/components/common/BioMobile.js
+++ public/src/components/common/BioMobile.js
@@ -1,7 +1,14 @@
 import React from "react";
 import { Image } from 'cloudinary-react';
 import Modal from 'antd/lib/modal';
+import styled from 'styled-components';
 
+const A = styled.a`
+  color: #C2C979;
+  &:hover {
+    cursor: pointer;
+  }
+`;
 
 export class Bio extends React.Component {
   constructor(props){
@@ -38,7 +45,7 @@ export class Bio extends React.Component {
           <div className="col-sm-6" style={{ marginTop: '3%', marginLeft: '3%', paddingBottom: '3%' }}>
             <h1 style={{ color: 'white', fontSize: '30px', letterSpacing: '4.5px', marginLeft: '2%', fontFamily: 'Oswald', paddingTop: '3%', margin: '0 auto', textAlign: 'center', marginBottom: '5%' }}><span style={{ color: '#3D8EE2' }}>about</span> <br /></h1>
             <p style={{ fontSize: '14px', textAlign: 'justify', marginRight: '7%', marginLeft: '7%', marginBottom: '3%' }}>Victoria Kurzweg is a designer and developer specializing in branding and content enhancement. She has worked with global brands in luxury fashion and hospitality, as well as startups in Silicon Beach. Victoria works primarily with Sketch and InVision for wireframing and prototyping, and React.js and Node.js for building responsive websites and applications.</p>
-            <p style={{ fontSize: '14px', textAlign: 'justify', marginRight: '7%', marginLeft: '7%', marginBottom: '3%' }}>With a background in writing, Victoria approaches her work with an editorial eye and an appreciation for good storytelling. She was born in New Orleans and graduated with a master's degree in history of international relations from the London School of Economics. Her interests include food and drink, fashion, travel and contemporary art.</p>
+            <p style={{ fontSize: '14px', textAlign: 'justify', marginRight: '7%', marginLeft: '7%', marginBottom: '3%' }}>With a <A href="/resume" target="blank">background</A> in writing, Victoria approaches her work with an editorial eye and an appreciation for good storytelling. She was born in New Orleans and graduated with a master's degree in history of international relations from the London School of Economics. Her interests include food and drink, fashion, travel and contemporary art.</p>
             <button onClick={this.showModal} style={{ textDecoration: 'none', backgroundColor: '#108EE9', color: '#212121', width: '50%', height: '3em', margin: '0 auto', border: 'none', textTransform: 'uppercase', letterSpacing: '1px', borderRadius: '2px', fontSize: '18px', fontWeight: 'bold' }}>Let's talk</button>
           </div>
         </div>
